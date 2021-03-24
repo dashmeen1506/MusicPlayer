dff0a1568ef199b1fe79775389afdb2a720eaa52
Dashmeen Singh <dashmeensingh@gmail.com>
2021-03-24 11:24:55 +0530

    added song_list.js

--- src/App.js
+++ src/App.js
@@ -1,41 +1,9 @@
 import React,{useState,useEffect} from 'react';
 import Player from './components/player';
 import 'bootstrap/dist/css/bootstrap.css';
+import songs from './songs/song_list'
 
 function App() {
-
-  const [songs] = useState([
-    {
-      title: "Closer",
-      artist: "The Chainsmokers ft.Hasley",
-      img_src: "./images/song-1.png", 
-      src: "./music/Closer.mp3",
-    },
-    {
-      title: "Not Afraid",
-      artist: "Eminem",
-      img_src: "./images/song-2.jpg", 
-      src: "./music/not_afraid.mp3"
-    },
-    {
-      title: "Rockstar",
-      artist: "Post Malone Parody",
-      img_src: "./images/song-3.jpg", 
-      src: "./music/Rockstar.mp3"
-    },
-    {
-      title: "Na Ja",
-      artist: "Pav Dharia",
-      img_src: "./images/song-4.jpg", 
-      src: "./music/naja.mp3"
-    },
-    {
-      title: "ROXANNE",
-      artist: "Arizona Zervas",
-      img_src: "./images/song-5.jpg", 
-      src: "./music/ROXANNE.mp3"
-    },
-  ]);
 
   const [currentSongIndex,setCurrentSongIndex]=useState(0);
   const [nextSongIndex,setNextSongIndex]=useState(0);
---
+++ src/songs/song_list.js
@@ -0,0 +1,34 @@
+const songs = [
+    {
+      title: "Closer",
+      artist: "The Chainsmokers ft.Hasley",
+      img_src: "./images/song-1.png", 
+      src: "./music/Closer.mp3",
+    },
+    {
+      title: "Not Afraid",
+      artist: "Eminem",
+      img_src: "./images/song-2.jpg", 
+      src: "./music/not_afraid.mp3"
+    },
+    {
+      title: "Rockstar",
+      artist: "Post Malone Parody",
+      img_src: "./images/song-3.jpg", 
+      src: "./music/Rockstar.mp3"
+    },
+    {
+      title: "Na Ja",
+      artist: "Pav Dharia",
+      img_src: "./images/song-4.jpg", 
+      src: "./music/naja.mp3"
+    },
+    {
+      title: "ROXANNE",
+      artist: "Arizona Zervas",
+      img_src: "./images/song-5.jpg", 
+      src: "./music/ROXANNE.mp3"
+    },
+];
+
+export default songs;
